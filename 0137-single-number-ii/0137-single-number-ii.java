78aa6599bc0281b845743e54bd45c6ac8a027f43
Vivek Shah <Vivekchiragshah2004@gmail.com>
2024-03-04 06:03:34 +0530

    Time: 1 ms (87.69%), Space: 44.9 MB (96.99%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; ++i) {
+            int sum = 0;
+            for (final int num : nums)
+                sum += num >> i & 1;
+            sum %= 3;
+            ans |= sum << i;
+        }
+        return ans;
+    }
+}
+
+// done
