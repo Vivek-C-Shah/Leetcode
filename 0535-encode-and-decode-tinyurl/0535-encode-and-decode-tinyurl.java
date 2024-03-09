c194e0437ee419e0d1bfde858de714735bf5c8ff
Vivek Shah <Vivekchiragshah2004@gmail.com>
2024-03-09 18:02:23 +0530

    Time: 3 ms (52.48%), Space: 42.9 MB (54.4%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,40 +1,24 @@
 public class Codec {
+    Map<String, String> codeDB = new HashMap<>(), urlDB = new HashMap<>();
+    static final String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    // Encodes a URL to a shortened URL.
-    public static String encode(String longUrl) {
-// //        https://leetcode.com/problems/design-tinyurl
-// //        http://tinyurl.com/4e9iAk
-//         StringBuilder sb = new StringBuilder();
-//         sb.append("http://tinyurl.com/");
-//         sb.append(helper(longUrl, true));
-//         return sb.toString();
-        return longUrl;
+    private String getCode() {
+        char[] code = new char[6];
+        for (int i = 0; i < 6; i++) 
+            code[i] = chars.charAt((int)(Math.random() * 62));
+        return "http://tinyurl.com/" + String.valueOf(code);
+    }
+    
+    public String encode(String longUrl) {
+        if (urlDB.containsKey(longUrl)) return urlDB.get(longUrl);
+        String code = getCode();
+        while (codeDB.containsKey(code)) code = getCode();
+        codeDB.put(code, longUrl);
+        urlDB.put(longUrl, code);
+        return code;
     }
 
-//    using caesar cipher
-
-    // public static String helper(String a, boolean b){
-    //     int key = 0;
-    //     if(b){
-    //         key = 5;
-    //     }
-    //     else {
-    //         key = -5;
-    //     }
-    //     StringBuilder sb = new StringBuilder();
-    //     for (int i = 0; i < a.length(); i++) {
-    //         sb.append((char) (a.charAt(i) + key));
-    //     }
-    //     return (sb.toString());
-    // }
-
-    // Decodes a shortened URL to its original URL.
-    public static String decode(String shortUrl) {
-        // return helper(shortUrl.substring(19), false);
-        return shortUrl;
+    public String decode(String shortUrl) {
+        return codeDB.get(shortUrl);
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(url));
