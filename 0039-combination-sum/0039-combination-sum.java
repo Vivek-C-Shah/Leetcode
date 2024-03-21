e0b14a4acdf4c7cfb6ac3f5bd536a15bac3a7ae1
Vivek Shah <Vivekchiragshah2004@gmail.com>
2024-03-21 11:59:08 +0530

    Time: 2 ms (84.01%), Space: 44.4 MB (66.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,21 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> lst = new ArrayList<>();
         Arrays.sort(candidates);
-        resComb(res, lst, candidates, target, 0);
+        combRecHelper(res, lst, candidates, target, 0);
         return res;
     }
-    public void resComb(List<List<Integer>> res, List<Integer> lst,int[] arr, int tar, int start){
-        if(tar == 0) {
+
+    public static  void combRecHelper(List<List<Integer>> res, List<Integer> lst, int[] arr, int tar, int start){
+        if(tar == 0){
             res.add(new ArrayList<>(lst));
             return;
         }
-        for (int j = start; j < arr.length && arr[j] <= tar; j++) {
-            lst.add(arr[j]);
-            resComb(res, lst, arr, tar-arr[j], j);
-            lst.remove(lst.size());
+        for (int i = start; i < arr.length && arr[i] <= tar; i++) {
+            lst.add(arr[i]);
+            combRecHelper(res, lst, arr, tar-arr[i], i);
+            lst.removeLast();
         }
     }
 }
