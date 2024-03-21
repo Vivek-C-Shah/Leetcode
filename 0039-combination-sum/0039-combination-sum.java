e37054dda24f88df33e0d125c686ba3c60fa2c29
Vivek Shah <Vivekchiragshah2004@gmail.com>
2024-03-21 11:46:50 +0530

    Time: 2 ms (84.01%), Space: 43.9 MB (99.35%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> lst = new ArrayList<>();
+        Arrays.sort(candidates);
+        resComb(res, lst, candidates, target, 0);
+        return res;
+    }
+    public void resComb(List<List<Integer>> res, List<Integer> lst,int[] arr, int tar, int start){
+        if(tar == 0) {
+            res.add(new ArrayList<>(lst));
+            return;
+        }
+        for (int j = start; j < arr.length && arr[j] <= tar; j++) {
+            lst.add(arr[j]);
+            resComb(res, lst, arr, tar-arr[j], j);
+            lst.remove(lst.size() - 1);
+        }
+    }
+}
